14a7dd531cd7f3134a29802bd8ad9738d5cc90f6
monil <mparikh_90@yahoo.com>
2018-02-01 00:14:15 -0800

    missing range correction

--- treesandmore/src/nontrees/PrintMissingRange.java
+++ treesandmore/src/nontrees/PrintMissingRange.java
@@ -1,45 +1,28 @@
 package nontrees;
 // failsed google question...shyaa
 public class PrintMissingRange {
-
+    
+    public void getRanges(int a[]) {
+        int missingLower = 0;
+        
+        for(int i=0;i<a.length;i++) {
+            if (a[i] == missingLower) {
+                missingLower++;
+            } else if (a[i] > missingLower) {
+                if (a[i] - missingLower > 1) {
+                    System.out.println(", " + missingLower + "-" + (a[i]-1));
+                } else {
+                    System.out.println(", " + missingLower);
+                }
+                missingLower = a[i]+1;
+            }
+        }
+    }
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         int a[] = {1, 3 ,9, 10,11,45,51};
-        Integer available[] = new Integer[100];
-        for(int i=0;i<available.length;i++) {
-            available[i] = -1;
-        }
-        for(int i=0;i<a.length;i++) {
-            available[a[i]] = 1;
-        }
-        int currentDiff = 0;
-        int minSoFar = 0;
-        int maxSoFar = 0;
-        for(int i=0;i<available.length;i++) {
-            if (available[i] == -1) {
-                currentDiff++;
-                if (minSoFar == 0) { // this means min not yer set;
-                    minSoFar = i;
-                }
-                maxSoFar = i;
-            } else {
-                if (currentDiff == 1) {
-                    System.out.print((i -1) + " , ");
-                } else if (currentDiff >1) {
-                    System.out.print(minSoFar + " - " + maxSoFar + " , ");
-                }
-                currentDiff = 0;
-                minSoFar = 0;
-                maxSoFar = 0;
-            }
-            if ( i == available.length-1) {
-                if (currentDiff == 1) {
-                    System.out.print((i -1) + " , ");
-                } else if (currentDiff >1) {
-                    System.out.print(minSoFar + " - " + maxSoFar + " , ");
-                }
-            }
-        }
+        PrintMissingRange obj = new PrintMissingRange();
+        obj.getRanges(a);
     }
 
 }
